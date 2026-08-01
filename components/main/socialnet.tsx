85a0dad3fb74f8e585406f0c2bf5dac0c194cf8a
Gekkolas <nikolasarango@outlook.com>
2026-08-01 16:14:54 -0500

    cv update

--- components/main/socialnet.tsx
+++ components/main/socialnet.tsx
@@ -52,7 +52,7 @@ export const Social = () => {
             </a>
           {/* cv */}
             <a
-            href="https://oumha429zaujvejc.public.blob.vercel-storage.com/NickLatestCV-D2AWjDoc85jZwNiyVQWpt77Gpk5qfi.pdf"
+            href="https://oumha429zaujvejc.public.blob.vercel-storage.com/CV%20Nicolas%20LastVersion2026.pdf"
             download
             className="flex justify-center items-center transition-transform duration-300 hover:scale-110 hover:text-pink-500"
             target="_blank"
